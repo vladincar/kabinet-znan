92ace9829e402c155befd89aae33d1715d7db86a
Vlad Levchenko <vladik@Vlads-Mac-mini.local>
2023-09-04 23:10:43 +0300

    done Hello section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./nav.css";
 import "./reset.css";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <>
       <Nav />
       <Header />
+      <Hello />
     </>
   );
 }
